fe6e3cf4221f871846c1a8cea22cd949b95e4c0b
A-makarim <syedmakarim0.2@gmail.com>
2025-12-20 01:10:25 +0000

    feat: uses annotate function to draw SAM3 output

---
+++ scripts/visualize_post_sam3.py
@@ -0,0 +1,17 @@
+"""
+visualise images with annotated labels
+"""
+
+from pathlib import Path
+import sys
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+if str(PROJECT_ROOT) not in sys.path:
+    sys.path.insert(0, str(PROJECT_ROOT))
+
+
+from src.annotate_labels_on_images import annotate_labels_on_images
+
+annotate_labels_on_images()
+
+print("Script says: Annotated images have been saved to the output directory.")
