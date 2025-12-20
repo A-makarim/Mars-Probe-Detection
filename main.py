a396786d7f53f39f00e73f4b23c77f57cf14dc77
A-makarim <syedmakarim0.2@gmail.com>
2025-12-20 04:35:43 +0000

    feat: grand file to run all scripts and generate YOLOv8n model weights from unlabelled video frames to detect non COCO objects

---
+++ main.py
@@ -0,0 +1,22 @@
+import subprocess
+import sys
+from pathlib import Path
+
+def run_script(path):
+    print(f"\n--- Running {path} ---")
+    subprocess.run(
+        [sys.executable, path],
+        check=True
+    )
+
+def main():
+    run_script("scripts/process_pre_sam3.py")
+    run_script("scripts/sam3_segmentation_via_images.py")
+    run_script("scripts/refine_pre_sam3.py")
+    run_script("scripts/augment_post_sam3.py")
+    run_script("scripts/visualize_post_sam3.py")
+    run_script("scripts/split_sam3_data.py")
+    run_script("scripts/yolo_training.py")
+
+if __name__ == "__main__":
+    main()
